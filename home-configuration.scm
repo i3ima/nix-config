528108e8e0cd630c457105a2b7ec3eee430f226b
i3ima <hueputalo.rubin@gmail.com>
2026-04-28 02:10:05 +0200

    feat: define dotfiles reference, add pins of some packages (node, python + pip), erlang/rebar3 variant without WxWidgets

--- home-configuration.scm
+++ home-configuration.scm
@@ -20,9 +20,52 @@
 
 
 
+;; My dotfiles with neo(vim) config as a submodule :3
+(define dotfiles-source
+  (origin
+    (method git-fetch)
+    (uri (git-reference
+           (url "https://github.com/i3ima/dotfiles.git")
+           (commit "77a8c0b1104f6fb17d92d25b8e0b91650ca3cb1e")
+           (recursive? #t)))
+    (file-name (git-file-name "dotfiles-recursive" "77a8c0"))
+    (sha256
+      (base32 "0p0llsaymdlpwfklvcv78c2dg9b3zdjjqzng7q89q5kfgk8qkl78"))))
+
+;; Erlang package variant to disable fucking WxWidgets that pulls whole GTK3 and GNOME stuff,
+;; which result in incredibly long compilation
+(define-public erlang-no-wx
+  (package
+    (inherit erlang)
+    (name "erlang")
+    (version "24.3.4.12")
+    (inputs
+      (modify-inputs (package-inputs erlang)
+        (delete "wxwidgets")))
+    (arguments (substitute-keyword-arguments (package-arguments erlang) 
+      ((#:configure-flags flags)
+       `(append ,flags
+                (list "--disable-wx")))))))
+
+;; Propagate to rebar3 variant, which has Erlang as dependency
+;; rebar3 itself needed for erlang_ls LSP server 
+(define rebar3-no-wx
+  (package/inherit rebar3
+    (name "rebar3-no-wx")
+    (native-inputs
+      (modify-inputs (package-native-inputs rebar3)
+        (replace "erlang" erlang-no-wx)))))
 
 
+;; Pin some packages versions
+(define node-22
+  (specification->package "node@22.14.0"))
 
+(define python-3.10
+  (specification->package "python@3.10.19"))
+
+(define python-pip-25
+  (specification->package "python-pip@25.1.1"))
 
 (home-environment
   ;; Below is the list of packages that will show up in your
