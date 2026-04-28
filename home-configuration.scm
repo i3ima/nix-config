7981c1d70d2eef4107b9a5756498a801e67f9b4a
i3ima <hueputalo.rubin@gmail.com>
2026-04-28 02:18:37 +0200

    chore: remove banner and export

--- home-configuration.scm
+++ home-configuration.scm
@@ -1,10 +1,4 @@
-;; This "home-environment" file can be passed to 'guix home reconfigure'
-;; to reproduce the content of your profile.  This is "symbolic": it only
-;; specifies package names.  To reproduce the exact same profile, you also
-;; need to capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
-(define-module (home-configuration)
-               #:export (nvim-config-source))
+(define-module (home-configuration))
 
 (use-modules (gnu home)
 	     (gnu packages)
